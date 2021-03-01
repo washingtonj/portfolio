e2037e38ebbff6bf4df5d116cad3a91c82a224c4
Washington Junior <washingtonj@outlook.com.br>
2021-02-28 23:24:18 -0300

    [NUX] - TailwindCSS extend.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '1xl': '1320px',
+      },
+    },
   },
   variants: {
     extend: {},
